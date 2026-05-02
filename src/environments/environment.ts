ff471ce7548de01e0cbfd41594314b27638a0b96
ANUBHAV-03042004 <114772861+ANUBHAV-03042004@users.noreply.github.com>
2026-05-02 18:52:06 +0530

    [Anubhav Kumar Srivastava] Add. pushed empty main

--- src/environments/environment.ts
+++
@@ -1,7 +0,0 @@
-export const environment = {
-  production: false,
-  apiBase: 'http://localhost:8080',
-  wsCollabEndpoint: 'http://localhost:8080/ws/collab/websocket',
-  wsNotificationEndpoint: 'http://localhost:8080/ws/notifications/websocket',
-  wsExecutionEndpoint: 'http://localhost:8080/ws/execution/websocket'
-};
